33a3b7f0d34a04e65c8a024a459c8b77c0f25ec6
Hasham Tanveer <hashamtanvr41@gmail.com>
2025-11-26 00:27:40 +0500

    Global variables and function prototypes declaration was committed

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+
+
+    // Global variable declaration
+    const int size1 = 9;
+    const int r = 9;
+    const int c = 9;
+
+    // Function prototypes
+    bool rowCheck(int arr[][size1], int , int);
+    bool columnCheck(int arr[][size1], int , int );
+    bool boxChecker(int arr[][size1], int , int , int);
+    void printArr(int arr[][size1], int i_a[][size1]);
+    bool isValid(int arr[][size1], int , int , int);
+
     return 0;
 }
