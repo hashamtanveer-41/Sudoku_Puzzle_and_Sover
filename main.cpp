e6a207f7d3185fe8657618ed3e9c09c50eb567d1
Hasham Tanveer <hashamtanvr41@gmail.com>
2025-11-26 00:34:55 +0500

    Print array function definition was committed

--- main.cpp
+++ main.cpp
@@ -46,125 +46,179 @@ bool columnCheck(int arr[][9], int num, int col){
     }
     return false;
 }
-bool boxChecker(int arr[][size1], int row, int col, int num){
-
+bool boxChecker(int arr[][size1], int row, int col, int num) {
     int box;
     bool boxChecker(int arr[][size1], int row, int col, int num){
 
-    int box;
-    if (row<= 2 && row>= 0 && col>= 0 && col <= 2)
-    {
-        box = 1;
-        for (int i = 0; i <= 2; i++){
-            for (int j = 0; j <= 2; j++){
-                if (num == arr[i][j]){
-                    return true;
+        int box;
+        if (row<= 2 && row>= 0 && col>= 0 && col <= 2)
+        {
+            box = 1;
+            for (int i = 0; i <= 2; i++){
+                for (int j = 0; j <= 2; j++){
+                    if (num == arr[i][j]){
+                        return true;
+                    }
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    if (row <= 2 && row >= 0 && col > 2 && col <= 5){
-        box = 2;
-        for (int i = 0; i <= 2; i++){
-            for (int j = 3; j <= 5; j++){
-                if (num == arr[i][j]){
-                    return true;
+        if (row <= 2 && row >= 0 && col > 2 && col <= 5){
+            box = 2;
+            for (int i = 0; i <= 2; i++){
+                for (int j = 3; j <= 5; j++){
+                    if (num == arr[i][j]){
+                        return true;
+                    }
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    if (row <= 2 && row >= 0 && col > 5 && col <= 8){
-        box = 3;
-        for (int i = 0; i <= 2; i++){
-            for (int j = 6; j <= 8; j++){
-                if (num == arr[i][j]){
-                    return true;
+        if (row <= 2 && row >= 0 && col > 5 && col <= 8){
+            box = 3;
+            for (int i = 0; i <= 2; i++){
+                for (int j = 6; j <= 8; j++){
+                    if (num == arr[i][j]){
+                        return true;
+                    }
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    //pt2
-    if (row <= 5 && row > 2 && col >= 0 && col <= 2)
-    {
-        box = 4;
-        for (int i = 3; i <= 5; i++){
-            for (int j = 0; j <= 2; j++){
-                if (num == arr[i][j]){
-                    return true;
+        //pt2
+        if (row <= 5 && row > 2 && col >= 0 && col <= 2)
+        {
+            box = 4;
+            for (int i = 3; i <= 5; i++){
+                for (int j = 0; j <= 2; j++){
+                    if (num == arr[i][j]){
+                        return true;
+                    }
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    if (row <= 5 && row > 2 && col > 2 && col <= 5)
-    {
-        box = 5;
-        for (int i = 3; i <= 5; i++){
-            for (int j = 3; j <= 5; j++){
-                if (num == arr[i][j]){
-                    return true;
+        if (row <= 5 && row > 2 && col > 2 && col <= 5)
+        {
+            box = 5;
+            for (int i = 3; i <= 5; i++){
+                for (int j = 3; j <= 5; j++){
+                    if (num == arr[i][j]){
+                        return true;
+                    }
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    if (row <= 5 && row > 2 && col > 5 && col <= 8)
-    {
-        box = 6;
-        for (int i = 3; i <= 5; i++){
-            for (int j = 6; j <= 8; j++){
-                if (num == arr[i][j]){
-                    return true;
+        if (row <= 5 && row > 2 && col > 5 && col <= 8)
+        {
+            box = 6;
+            for (int i = 3; i <= 5; i++){
+                for (int j = 6; j <= 8; j++){
+                    if (num == arr[i][j]){
+                        return true;
+                    }
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    //pt3
-    if (row <= 8 && row > 5 && col >= 0 && col <= 2){
-        box = 7;
-        for (int i = 6; i <= 8; i++){
-            for (int j = 0; j <= 2; j++){
-                if (num == arr[i][j]){
-                    return true;
+        //pt3
+        if (row <= 8 && row > 5 && col >= 0 && col <= 2){
+            box = 7;
+            for (int i = 6; i <= 8; i++){
+                for (int j = 0; j <= 2; j++){
+                    if (num == arr[i][j]){
+                        return true;
+                    }
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    if (row <= 8 && row > 5 && col > 2 && col <= 5)
-    {
-        box = 8;
-        for (int i = 6; i <= 8; i++){
-            for (int j = 3; j <= 5; j++){
-                if (num == arr[i][j]){
-                    return true;
+        if (row <= 8 && row > 5 && col > 2 && col <= 5)
+        {
+            box = 8;
+            for (int i = 6; i <= 8; i++){
+                for (int j = 3; j <= 5; j++){
+                    if (num == arr[i][j]){
+                        return true;
+                    }
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    if (row <= 8 && row > 5 && col > 5 && col <= 8){
-        box = 9;
-        for (int i = 6; i <= 8; i++){
-            for (int j = 6; j <= 8; j++){
-                if (num == arr[i][j]){
-                    return true;
+        if (row <= 8 && row > 5 && col > 5 && col <= 8){
+            box = 9;
+            for (int i = 6; i <= 8; i++){
+                for (int j = 6; j <= 8; j++){
+                    if (num == arr[i][j]){
+                        return true;
+                    }
                 }
             }
+            return false;
         }
         return false;
     }
-    return false;
+}
+
+void printArr(int arr[][size1], int i_a[][size1]){
+    cout << "\n";
+    cout << "\n";
+    cout << "\t";
+    cout << "   ";
+    for (int i = 1; i <= size1; i++) {
+        cout << i << " ";
+        if (i == 3){
+            cout <<"  ";
+        }
+        if (i == 6) {
+            cout << "  ";
+        }
+    }
+    cout << "\n";
+    cout << "\t";
+
+    for (int i = 0; i < 25; i++){
+        cout << "_";
+    }
+    cout << endl;
+    cout << "\t";
+    for (int i = 0; i < 25; i++){
+        cout << "-";
+    }
+    cout << "\n";
+
+    for (int i = 0; i < size1; i++){
+        cout << "\t" << i + 1;
+        cout << "| ";
+
+        for (int j = 0; j < 9; j++){
+            cout << arr[i][j] << " ";
+            if (j == 2 || j == 5 || j == 8){
+                cout << "| ";
+            }
+        }
+        cout << endl;
+
+        if (i == 2 || i == 5 || i == 8){
+            cout << "\t";
+            for (int i = 0; i <= 25; i++){
+                cout << "_";
+            }
+            cout << endl;
+            cout << "\t";
+            for (int i = 0; i < 25; i++){
+                cout << "-";
+            }
+            cout << "\n";
+        }
+    }
 }
