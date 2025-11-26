a8ad89e9fdcca3d754bc9b03df1c1591312e8efd
Hasham Tanveer <hashamtanvr41@gmail.com>
2025-11-26 18:39:04 +0500

    Column check and row check functions were modified

--- main.cpp
+++ main.cpp
@@ -8,40 +8,44 @@ using namespace std;
 const int size1 = 9;
 
 // Function prototypes
-bool rowCheck(int arr[][size1], int , int);
-bool columnCheck(int arr[][size1], int , int );
-bool boxChecker(int arr[][size1], int , int , int);
+bool rowCheck(int arr[][size1], int row, int num);
+bool columnCheck(int arr[][size1], int col, int num);
+bool boxChecker(int arr[][size1], int row, int col, int num);
+bool isValid(int arr[][size1], int row, int col, int num);
+bool solveSudoku(int arr[][size1], int row, int col);
 void printArr(int arr[][size1]);
-bool isValid(int arr[][size1], int , int , int);
 void randomNumbers(int nums[]);
 void makePuzzle(int arr[][size1], int holes);
-bool solveSudoku(int arr[][size1], int row, int col) ;
+
+
 int main() {
     srand(time(0));
+
     int board[size1][size1] = {0};
+
     solveSudoku(board, 0, 0); // generate solved board
+
     printArr(board);
+
     makePuzzle(board, 40); // remove 40 cells for puzzle
+
     printArr(board);
+
     return 0;
 }
 
-bool rowCheck(int arr[][9], int row, int num) {
-    for (int col= 0; col< 9 ; col++) {
-        if (num ==arr[row][col]) {
-            return true;
-        }
-    }
-    return false;
+bool rowCheck(int arr[][size1], int row, int num) {
+    for (int col = 0; col < 9; col++)
+        if (arr[row][col] == num)
+            return false;
+    return true;
 }
 
-bool columnCheck(int arr[][9], int num, int col){
-    for ( int row= 0; row< 9 ; row++) {
-        if (num ==arr[row][col]) {
-            return true;
-        }
-    }
-    return false;
+bool columnCheck(int arr[][size1], int col, int num) {
+    for (int row = 0; row < 9; row++)
+        if (arr[row][col] == num)
+            return false;
+    return true;
 }
 bool boxChecker(int arr[][size1], int row, int col, int num) {
     int startRow = row - row % 3;
@@ -51,10 +55,12 @@ bool boxChecker(int arr[][size1], int row, int col, int num) {
         for (int j = 0; j < 3; j++)
             if (arr[startRow + i][startCol + j] == num)
                 return false;
+
     return true;
 }
 
 void printArr(int arr[][size1]) {
+    cout << "\nGenerated Sudoku:\n\n";
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             cout << arr[i][j] << " ";
@@ -82,7 +88,7 @@ void makePuzzle(int arr[][size1], int holes) {
         if (arr[r][c] != 0)
             arr[r][c] = 0;
         else
-            holes++;
+            holes++;   // try again
     }
 }
 bool solveSudoku(int arr[][size1], int row, int col) {
@@ -94,7 +100,8 @@ bool solveSudoku(int arr[][size1], int row, int col) {
         return solveSudoku(arr, row, col + 1);
 
     int nums[9];
-    randomNumbers(nums);
+    randomNumbers(nums);   // <-- shuffle numbers each call
+
     for (int i = 0; i < 9; i++) {
         int num = nums[i];
 
@@ -105,6 +112,7 @@ bool solveSudoku(int arr[][size1], int row, int col) {
                 return true;
         }
     }
+
     arr[row][col] = 0;
     return false;
 }
